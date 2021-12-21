81597fabdfa86a698c538d1683e5aa6cac624738
Felix <felicitusneko@vivaldi.net>
2021-12-21 15:32:06 -0400

    day 21

--- src/Advent.ts
+++ src/Advent.ts
@@ -7,142 +7,8 @@ import BITSReader, {
 } from "./BITSReader";
 
 const useTestData = false;
-const testData = `--- scanner 0 ---
-404,-588,-901
-528,-643,409
--838,591,734
-390,-675,-793
--537,-823,-458
--485,-357,347
--345,-311,381
--661,-816,-575
--876,649,763
--618,-824,-621
-553,345,-567
-474,580,667
--447,-329,318
--584,868,-557
-544,-627,-890
-564,392,-477
-455,729,728
--892,524,684
--689,845,-530
-423,-701,434
-7,-33,-71
-630,319,-379
-443,580,662
--789,900,-551
-459,-707,401
-
---- scanner 1 ---
-686,422,578
-605,423,415
-515,917,-361
--336,658,858
-95,138,22
--476,619,847
--340,-569,-846
-567,-361,727
--460,603,-452
-669,-402,600
-729,430,532
--500,-761,534
--322,571,750
--466,-666,-811
--429,-592,574
--355,545,-477
-703,-491,-529
--328,-685,520
-413,935,-424
--391,539,-444
-586,-435,557
--364,-763,-893
-807,-499,-711
-755,-354,-619
-553,889,-390
-
---- scanner 2 ---
-649,640,665
-682,-795,504
--784,533,-524
--644,584,-595
--588,-843,648
--30,6,44
--674,560,763
-500,723,-460
-609,671,-379
--555,-800,653
--675,-892,-343
-697,-426,-610
-578,704,681
-493,664,-388
--671,-858,530
--667,343,800
-571,-461,-707
--138,-166,112
--889,563,-600
-646,-828,498
-640,759,510
--630,509,768
--681,-892,-333
-673,-379,-804
--742,-814,-386
-577,-820,562
-
---- scanner 3 ---
--589,542,597
-605,-692,669
--500,565,-823
--660,373,557
--458,-679,-417
--488,449,543
--626,468,-788
-338,-750,-386
-528,-832,-391
-562,-778,733
--938,-730,414
-543,643,-506
--524,371,-870
-407,773,750
--104,29,83
-378,-903,-323
--778,-728,485
-426,699,580
--438,-605,-362
--469,-447,-387
-509,732,623
-647,635,-688
--868,-804,481
-614,-800,639
-595,780,-596
-
---- scanner 4 ---
-727,592,562
--293,-554,779
-441,611,-461
--714,465,-776
--743,427,-804
--660,-479,-426
-832,-632,460
-927,-485,-438
-408,393,-506
-466,436,-512
-110,16,151
--258,-428,682
--393,719,612
--211,-452,876
-808,-476,-593
--575,615,604
--485,667,467
--680,325,-822
--627,-443,-432
-872,-547,-609
-833,512,582
-807,604,487
-839,-516,451
-891,-625,532
--652,-548,-490
-30,-46,-14`;
+const testData = `Player 1 starting position: 4
+Player 2 starting position: 8`;
 
 // Day 4
 interface BingoBoard {
@@ -197,6 +63,7 @@ interface Beacon {
   relBeaconDistance: number[][];
 }
 
+// General utility
 const getInput = (year: number, day: number) => {
   if (useTestData && testData !== null) return testData;
   const { USERAGENT, COOKIE } = process.env;
@@ -245,6 +112,7 @@ export default class Advent {
       [this.Day18Problem1, this.Day18Problem2],
       [this.Day19Problem1, this.Day19Problem2],
       [this.Day20Problem1, this.Day20Problem2],
+      [this.Day21Problem1, this.Day21Problem2],
     ];
   }
 
@@ -2102,5 +1970,87 @@ export default class Advent {
 
   async Day20Problem2(data: string) {
     return this.Day20(data, 50);
+  }
+
+  // Day 21
+  async Day21Problem1(data: string) {
+    const startPos = data
+      .trim()
+      .split(/\n/)
+      .map((i) => {
+        const posData = /Player \d starting position: (\d+)/.exec(i);
+        if (!posData) throw new Error("Error reading starting position data");
+        return Number.parseInt(posData[1]);
+      });
+
+    const dice = [0, 1, 2];
+    const score = [0, 0];
+    const pos = startPos.slice().map((i) => i - 1);
+    let totalRolls = 0;
+
+    let done = false;
+    while (!done) {
+      for (const x in startPos) {
+        pos[x] = (pos[x] + dice.reduce((r, i) => r + i) + 3) % 10;
+        score[x] += pos[x] + 1;
+        totalRolls += 3;
+        for (const y in dice) dice[y] = (dice[y] + 3) % 100;
+        if (score[x] >= 1000) {
+          done = true;
+          break;
+        }
+      }
+    }
+    // console.debug(score, totalRolls);
+
+    return Math.min(...score) * totalRolls;
+  }
+
+  async Day21Problem2(data: string) {
+    const startPos = data
+      .trim()
+      .split(/\n/)
+      .map((i) => {
+        const posData = /Player \d starting position: (\d+)/.exec(i);
+        if (!posData) throw new Error("Error reading starting position data");
+        return Number.parseInt(posData[1]);
+      });
+
+    const odds: Record<number, number> = {};
+    for (let x = 0; x < 27; x++) {
+      const result =
+        [x % 3, Math.floor(x / 3) % 3, Math.floor(x / 9) % 3].reduce(
+          (r, i) => r + i
+        ) + 3;
+      odds[result] === undefined ? (odds[result] = 1) : odds[result]++;
+    }
+
+    const wins = [0, 0];
+    const RecurseReality = (
+      score: number[],
+      pos: number[],
+      splits: number[],
+      curPlayer: number
+    ) => {
+      for (const roll of Object.keys(odds).map(i => Number.parseInt(i))) {
+        const newScore = score.slice();
+        const newPos = pos.slice();
+        const newSplits = splits.slice();
+        
+        newPos[curPlayer] = (newPos[curPlayer] + roll) % 10;
+        newScore[curPlayer] += newPos[curPlayer] + 1;
+        newSplits.push(odds[roll]);
+
+        if (newScore[curPlayer] >= 21) {
+          wins[curPlayer] += newSplits.reduce((r,i) => r*i);
+        } else {
+          RecurseReality(newScore, newPos, newSplits, curPlayer === 0 ? 1 : 0);
+        }
+        if (splits.length === 0) console.debug('Wins so far:', wins);
+      }
+    };
+    RecurseReality([0,0], startPos.slice().map(i => i-1), [], 0);
+
+    return Math.max(...wins);
   }
 }
